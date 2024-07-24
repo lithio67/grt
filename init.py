e0927952b3b35b0f6470fde95b54ac6f714d12c5
lithio67 <shliu0607@gmail.com>
2024-07-24 11:40:49 -0500

    Add files via upload

---
+++ init.py
@@ -0,0 +1,15 @@
+import random
+import numpy
+def greetings():
+    print("Hello World")
+
+def say(*args, **kwargs):
+    print(*args, **kwargs)
+
+ran = random.random
+
+def arr(*args, **kwargs):
+    array(*args, **kwargs)
+arr()
+
+
