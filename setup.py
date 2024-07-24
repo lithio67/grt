5cfe9ae3c2cd5685c51c830d47c13e8695f11ba9
lithio67 <shliu0607@gmail.com>
2024-07-24 11:26:44 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grt library",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",  # Required for array functionality
+    ],
+    author="Aidan Liu",
+    description="A simplified library with custom function names",
+    long_description="This library provides simplified function names: say() for print(), ran() for random(), and arr() for array().",
+    long_description_content_type="text/plain",
+    url="https://github.com/yourusername/my_simplified_library",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
